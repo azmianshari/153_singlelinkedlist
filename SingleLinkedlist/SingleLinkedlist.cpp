d9751887c541312289c213336f723767d38df8a1
azmianshari <azmianshari123@gmail.com>
2023-05-19 12:40:59 +0700

    Membuat searchNode

--- SingleLinkedlist/SingleLinkedlist.cpp
+++ SingleLinkedlist/SingleLinkedlist.cpp
@@ -15,5 +15,76 @@ void addNode() {
 	Node* nodeBaru = new Node();
 	cout << "Masukan NIM :";
 	cin >> nim;
-	cout
+	cout << "Masukan Nama :";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	nodeBaru->name = nama;
+
+	if (START == NULL || nim <= START->noMhs) {
+		if (START != NULL && nim == START->noMhs) {
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+	Node* previous = START;
+	Node* current = START;
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs) {
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
+	}
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+	
 }
+
+bool searchNode(int nim, Node* current, Node* previous) {
+	previous = START;
+	current = START;
+	while (current != NULL && nim > current -> noMhs) {
+		previous = current;
+		current = current->next;
+	}
+	if (current == NULL) 
+	{
+		return false;
+	}
+	else if (current->noMhs == nim) 
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool deleteNode(int nim) {
+	Node* current = START;
+	Node* previous = START;
+	if (searchNode(nim, previous, current) == false)
+		return false;
+	previous->next = current->next;
+	if (current == START)
+		START = current->next;
+	if (current == START)
+		START = current->next;
+	return true;
+}
+bool listEmpty() {
+	if (start == NULL)
+		return true;
+	else
+		return false;
+}
+void
+
+
